35af2b661c9155b272c04f8fe2deacc246c2fb69
zeynepcakm <zynpckmk8@gmail.com>
2024-08-02 03:12:03 -0400

    US06 is completed

--- src/test/java/com/bluecrm/step_definitions/EmployeesPage_stepDef_ZC.java
+++ src/test/java/com/bluecrm/step_definitions/EmployeesPage_stepDef_ZC.java
@@ -1,9 +1,18 @@
 package com.bluecrm.step_definitions;
 
 import com.bluecrm.pages.EmployeesPage_ZC;
+import com.bluecrm.utilities.BrowserUtils;
 import com.bluecrm.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
+import java.util.List;
 
 public class EmployeesPage_stepDef_ZC {
 
@@ -15,8 +24,30 @@ public class EmployeesPage_stepDef_ZC {
     }
 
 
-    @Then("users see following modules")
-    public void users_see_following_modules() {
+    @Then("users see following {int} modules")
+    public void usersSeeFollowingModules(int expectedModuleNumber, List<String> expectedModules) {
 
+        List<String> actualModules = BrowserUtils.getElementsText(employeesPageZc.employeeModules);
+
+        Assert.assertEquals(expectedModuleNumber, actualModules.size());
+        Assert.assertEquals(expectedModules, actualModules);
+    }
+
+
+    @When("the user clicks on Add Department button")
+    public void theUserClicksOnAddDepartmentButton() {
+        employeesPageZc.addDepartmentButton.click();
+    }
+
+    @Then("the user can add department")
+    public void theUserCanAddDepartment() {
+        employeesPageZc.departmentNameInputBox.sendKeys("Instructors Department");
+
+        employeesPageZc.addButton.click();
+    }
+
+    @And("the user can see the department in the company structure system")
+    public void theUserCanSeeTheDepartmentInTheCompanyStructureSystem() {
+        employeesPageZc.newDepartment.isDisplayed();
     }
 }
--- src/test/resources/features/US06_employees_page_ZC.feature
+++ src/test/resources/features/US06_employees_page_ZC.feature
@@ -8,7 +8,7 @@ Feature: Users should be able to access to the Employees page
     Given the user logged in as "<userType>"
 #    Given the user logged in with username as "User1" and password as "UserUser123"
     When users click on the Employees page
-    Then users see following modules
+    Then users see following 8 modules
       | Company Structure   |
       | Find Employee       |
       | Telephone Directory |
@@ -23,3 +23,15 @@ Feature: Users should be able to access to the Employees page
       | hr                |
       | help desk         |
       | marketing manager |
+
+
+    Scenario Outline: Verify users can add department on the Employees page.
+      Given the user logged in as "<userType>"
+      When the user clicks on Add Department button
+      Then the user can add department
+      And the user can see the department in the company structure system
+      Examples:
+        | userType          |
+        | hr                |
+        | help desk         |
+        | marketing manager |
