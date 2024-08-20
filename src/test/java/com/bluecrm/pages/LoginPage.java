2ff242f6260072e91d84943af79599673a8292e3
dilovar1992 <dellovar56@gmail.com>
2024-08-20 07:41:35 -0400

    new webelements are added

--- src/test/java/com/bluecrm/pages/LoginPage.java
+++ src/test/java/com/bluecrm/pages/LoginPage.java
@@ -7,18 +7,28 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
-    public LoginPage(){
+    public LoginPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(name="USER_LOGIN")
+    @FindBy(name = "USER_LOGIN")
     public WebElement userName;
 
-    @FindBy(name="USER_PASSWORD")
+    @FindBy(name = "USER_PASSWORD")
     public WebElement password;
 
     @FindBy(css = "input[type='submit']")
     public WebElement loginButton;
+
+    @FindBy(id = "USER_REMEMBER")
+    public WebElement checkBox;
+    @FindBy(xpath = "//a[.='Forgot your password?']")
+    public WebElement forgotPassLink;
+    @FindBy(xpath = "//div[.='Get Password']")
+    public WebElement getPasswordHeader;
+    @FindBy(xpath = "//label[.='Remember me on this computer']")
+    public WebElement rememberMe;
+
 
     public void login(String userNameStr, String passwordStr) {
         userName.sendKeys(userNameStr);
